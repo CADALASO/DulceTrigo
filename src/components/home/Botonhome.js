21eeb4a6cf2e250bbc63f54ff53fa6d5686de6fd
CarenjDaza <carenjdaza120813@gmail.com>
2022-06-17 02:38:54 -0500

    se genera codigo del home

---
+++ src/components/home/Botonhome.js
@@ -0,0 +1,38 @@
+import './Botonhome.css';
+import hr from '../../assets/img/linea-nav.svg';
+import { Link } from 'react-router-dom';
+
+function Botonhome() {
+    return (
+        <div className='home'>
+            <h2>¿De que te antojaste hoy?</h2>
+            <img src={hr} alt="linea" className='hr'/>
+            <div className='buttons'>
+                <button className='boton1 cake'>
+                    <Link to="/galletas">
+                        <div>
+                            <span>
+                                <h1 className='thome'>Pasteleria</h1>
+                                <hr/>
+                            </span>
+                        </div>
+                    </Link>
+                </button>
+                <button className='boton2 bakery'>
+                    <Link to="/especial">
+                        <div>
+                            <span>
+                                <h1 className='thome'>Panaderia</h1>
+                                <hr/>
+                            </span>
+                        </div>
+                    </Link>
+                </button>   
+            </div>
+            <img src={hr} alt="linea" className='hrdown'/>
+        </div>
+
+    )
+}
+
+export default Botonhome;
